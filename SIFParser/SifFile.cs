adbdfb43fff1202a09f883cf55a4f6bb622fd3e0
Brandon Zuech <brandonzuech@hotmail.com>
2022-09-20 19:43:28 -0400

    init

---
+++ SIFParser/SifFile.cs
@@ -0,0 +1,28 @@
+﻿namespace SIFParser
+{
+    /// <summary>
+    /// S-, S%=<Discount Percentage, i.e.: Sell Price = PL ? (PL * S- * 0.01)>
+    /// P%, B%=<Purchase/Buy Percentage, i.e.: Purchase Cost = PL ? (PL * P % *0.01) >
+    /// </summary>
+
+    public class SIFFile
+    {
+        public string ProductNumber { get; set; }
+        public string ProductDescription { get; set; }
+        public string ManufacturerCode { get; set; }
+        public string SideMark { get; set; }
+        public string Quantity { get; set; }
+        public string ListPrice { get; set; }
+        public string OptionNumber { get; set; }
+        public string OptionDescription { get; set; }
+        public string AttributeNumber { get; set; }
+        public string AttributeDescription { get; set; }
+        public string ProductPicture { get; set; }
+        public string DesignManagerSalesCategory { get; set; }
+        public string ItemBudgetAmount { get; set; }
+        public string LocationCode { get; set; }
+        public string LocationName { get; set; }
+        public string ManufacturerName { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ SIFReader/Program.cs
@@ -0,0 +1,22 @@
+﻿using SIFParser;
+
+Console.WriteLine("Reading file");
+
+var reader = new SIFReader();
+
+var sifs = await reader.Read("pathToFile.sif");
+
+foreach (var sif in sifs)
+{
+    Console.WriteLine("ProductNumber: " + sif.ProductNumber);
+    Console.WriteLine("Product Description: " + sif.ProductDescription);
+    Console.WriteLine("Color: " + sif.Color);
+    Console.WriteLine("Quantity: " + sif.Quantity);
+    Console.WriteLine("Manufacturer: " + sif.ManufacturerName + " (" + sif.ManufacturerCode + ")");
+    Console.WriteLine("Option Description: " + sif.OptionDescription);
+    Console.WriteLine("");
+    Console.WriteLine("");
+
+}
+
+Console.WriteLine("Done reading file");
